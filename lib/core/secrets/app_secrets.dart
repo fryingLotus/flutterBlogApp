182d9cbc20813054a45adac16f41f3b2d3bb6004
fryingLotus <blightningboltz5@gmail.com>
2024-11-11 18:38:04 +0700

    Removed secret codes

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,4 +1,5 @@
 class AppSecrets {
+  static const test = "";
   static const supabaseUrl = 'https://wcaocknlcwrerzdjkkok.supabase.co';
   static const supabaseAnonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjYW9ja25sY3dyZXJ6ZGpra29rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI4NjUwNjQsImV4cCI6MjAzODQ0MTA2NH0.XHHIqsYY-nA3YNbuxCAS0x8WE2ky95vjiLqcFjljZFs';
