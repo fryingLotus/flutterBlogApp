8c775fdbfa75ad9261a9607359dc01f6ed54df0c
fryingLotus <blightningboltz5@gmail.com>
2024-12-30 00:08:50 +0700

    stored secret in .env file instead

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,6 @@
+class AppSecrets {
+  static const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+  static const supabaseAnonKey = String.fromEnvironment('SUPABASE_ANON_KEY');
+  static const webClientId = String.fromEnvironment('WEB_CLIENT_ID');
+  static const iosClientId = String.fromEnvironment('IOS_CLIENT_ID');
+}
